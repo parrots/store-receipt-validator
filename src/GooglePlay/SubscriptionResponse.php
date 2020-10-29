d503b8fc83cdcdaf5f25385266e4f52221db38bf
Curtis Herbert <me@curtisherbert.com>
2020-10-29 11:55:57 -0400

    Update SubscriptionResponse.php

--- src/GooglePlay/SubscriptionResponse.php
+++ src/GooglePlay/SubscriptionResponse.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 namespace ReceiptValidator\GooglePlay;
 
 /**
@@ -59,6 +61,20 @@ class SubscriptionResponse extends AbstractResponse
     {
         return $this->response->getStartTimeMillis();
     }
+    
+    /**
+     * @return Carbon|null
+     */
+    public function getStartDate(): ?Carbon
+    {
+        if (null !== $this->response->getStartTimeMillis()) {
+            return Carbon::createFromTimestampUTC(
+                (int) round((int) $this->response->getStartTimeMillis() / 1000)
+            );
+        }
+        
+        return null;
+    }
 
     /**
      * @return int
@@ -67,6 +83,20 @@ class SubscriptionResponse extends AbstractResponse
     {
         return $this->response->getExpiryTimeMillis();
     }
+    
+    /**
+     * @return Carbon|null
+     */
+    public function getExpiryDate(): ?Carbon
+    {
+        if (null !== $this->response->getExpiryTimeMillis()) {
+            return Carbon::createFromTimestampUTC(
+                (int) round((int) $this->response->getExpiryTimeMillis() / 1000)
+            );
+        }
+        
+        return null;
+    }
 
     /**
      * @return int|null
@@ -74,6 +104,20 @@ class SubscriptionResponse extends AbstractResponse
     public function getUserCancellationTimeMillis()
     {
         return $this->response->getUserCancellationTimeMillis();
+    }
+    
+    /**
+     * @return Carbon|null
+     */
+    public function getCancellationDate(): ?Carbon
+    {
+        if (null !== $this->response->getUserCancellationTimeMillis()) {
+            return Carbon::createFromTimestampUTC(
+                (int) round((int) $this->response->getUserCancellationTimeMillis() / 1000)
+            );
+        }
+        
+        return null;
     }
 
     /**
